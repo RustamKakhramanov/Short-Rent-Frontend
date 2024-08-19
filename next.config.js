8444d6db88028e8db91001b10a55ef5fb2428c00
rustam.k <rusik.19101997@gmail.com>
2024-08-19 10:31:34 +0500

    next config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   swcMinify: true,
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:8081/api' // development api
+        ? 'http://localhost:8082/api' // development api
         : '/api' // production api
   },
   images: {
